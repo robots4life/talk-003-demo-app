4363b14cfa5b688f02de4be31cb3b6c3fdce6890
robots4life <34193243+robots4life@users.noreply.github.com>
2023-08-22 13:40:47 +0200

    slide-3

---
+++ src/routes/contact/+page.server.js
@@ -0,0 +1,28 @@
+export const load = async ({ params, route, url }) => {
+	//
+	// check the server console
+	console.log('\n');
+	console.log('-------------------------------------------');
+	console.log(new Date());
+	console.log('\n');
+	console.log('url : ', url);
+	console.log('\n');
+	console.log('route : ', route);
+	console.log('\n');
+	console.log('params : ', params);
+
+	// let urlData = JSON.stringify(url);
+
+	// return {
+	// 	urlDataString: urlData,
+	// 	routeData: route,
+	// 	...params,
+	// 	href: url.href,
+	// 	origin: url.origin,
+	// 	protocol: url.protocol,
+	// 	hostname: url.hostname,
+	// 	port: url.port,
+	// 	pathname: url.pathname,
+	// 	searchParams: url.searchParams.get('search')
+	// };
+};
--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,11 @@
+<script>
+	export let data;
+</script>
+
 <h1>Contact</h1>
 
 <a href="/">Home</a>
+
+<p>1. Check the server console to see what is happening.</p>
+<p>2. Return URL, ROUTE and PARAMS from the load function to the page.</p>
+<pre>{JSON.stringify(data, null, 2)}</pre>
