0ec365547896f2c7154c78dbc8beeb43e11b8dc9
robots4life <34193243+robots4life@users.noreply.github.com>
2023-08-22 13:08:01 +0200

    slide-2

---
+++ src/routes/about/+page.server.js
@@ -0,0 +1,8 @@
+// load function
+// https://kit.svelte.dev/docs/load#page-data
+export const load = () => {
+	// this data is loaded inside the load function and returned to the page
+	return {
+		key: 'value'
+	};
+};
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,3 +1,14 @@
+<script>
+	// https://kit.svelte.dev/docs/load#page-data
+	// we get the data for the page inside the "data" property
+	export let data;
+	console.log(data);
+</script>
+
 <h1>About</h1>
 
 <a href="/">Home</a>
+
+<pre>{data}</pre>
+
+<pre>{JSON.stringify(data)}</pre>
