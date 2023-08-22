ec2253330671969701e8b3406ea89402069ffe65
robots4life <34193243+robots4life@users.noreply.github.com>
2023-08-22 18:08:04 +0200

    slide-2

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,16 @@
 <h1>Welcome to SvelteKit</h1>
 
-<a href="/about">About</a>
+<div class="flex">
+	<a href="/about">About</a>
 
-<a href="/contact">Contact</a>
+	<a href="/contact">Contact</a>
 
-<a href="/work">Work</a>
+	<a href="/work">Work</a>
+</div>
+
+<style>
+	.flex {
+		display: flex;
+		gap: 2rem;
+	}
+</style>
